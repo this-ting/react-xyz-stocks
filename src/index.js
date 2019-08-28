8faca97d43729b5d55627150b833abbc51125224
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-08-28 11:52:03 +0800

    Set up babel plugin (plugin-proposal-class-properties), React Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Router, Link } from "react-router-dom";
 
 
 class App extends Component {
+  state = {
+    name: 'ting'
+  }
+  
   render() {
     return (
       <div className='container'>
-        <h1>Hello XYZ Stocks</h1>
+        <h1>This is XYZ Stocks. Hello {this.state.name}</h1>
       </div>
     )
   }
