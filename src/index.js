65e17ffac512b97f8fbdc3dcd2a60bbc7d2d28a6
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-08-28 11:37:59 +0800

    Set up react & JSX with Babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('start project')
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Hello XYZ Stocks</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,8 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'], },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ],
   },
 
