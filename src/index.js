890b883ea46b8cd40bb471237e076dc1aee6df67
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-08-29 17:55:18 +0800

    Install Material-UI

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
   state = {
     name: 'ting'
-  }
-  
+  };
+
   render() {
     return (
-      <div className='container'>
+      <div>
+        <CssBaseline />
         <h1>This is XYZ Stocks. Hello {this.state.name}</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
       </div>
-    )
+    );
   }
 }
 
