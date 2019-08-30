0ecc8fa37e8ba587fbc631f41e75a43fb0b5377a
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-08-30 15:02:59 +0800

    Set up initial header

---
+++ src/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { AppBar, Tab, Tabs } from '@material-ui/core';
+import Home from '../Home/Home.js';
+import Markets from '../Markets/Markets.js';
+import User from '../User/User.js';
+
+function Header() {
+  return (
+    <Router>
+      <AppBar position="static" className="header">
+        <Tabs variant="fullWidth">
+          <Tab label="Home" value="0" to="/" component={NavLink} />
+          <Tab label="Markets" value="1" to="/markets/" component={NavLink} />
+          <Tab label="User" value="2" to="/user/" component={NavLink} />
+        </Tabs>
+      </AppBar>
+      <Route path="/" exact component={Home} />
+      <Route path="/markets/" component={Markets} />
+      <Route path="/user/" component={User} />
+    </Router>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Home from './Home/Home.js';
-import Markets from './Markets/Markets.js';
-import User from './User/User.js';
+import Header from './Header/Header.js';
 
 class App extends Component {
   state = {
@@ -13,28 +10,10 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Router>
-          <CssBaseline />
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/markets/">Markets</Link>
-              </li>
-              <li>
-                <Link to="/user/">User</Link>
-              </li>
-            </ul>
-          </nav>
-          <h1>This is XYZ Stocks. Hello {this.state.name}</h1>
-          <Route path="/" exact component={Home} />
-          <Route path="/markets/" component={Markets} />
-          <Route path="/user/" component={User} />
-        </Router>
-      </div>
+      <>
+        <CssBaseline />
+        <Header />
+      </>
     );
   }
 }
