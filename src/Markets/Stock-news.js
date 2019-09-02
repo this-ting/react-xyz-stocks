bd23c29243ca39191f5be6a9ab9b82bfb07e13f6
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-02 18:35:25 +0800

    Console.log API for stock-company info

---
+++ src/Markets/Stock-news.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Paper, Grid } from '@material-ui/core';
+
+class News extends Component {
+  state = {
+    news: []
+  };
+
+  componentDidMount() {
+    const req = new XMLHttpRequest();
+    req.open(
+      'get',
+      `https://sandbox.iexapis.com/stable/stock/aapl/news/last/?token=Tpk_7190efa09280470180ab8bb6635da780`
+    );
+    req.send();
+    req.onload = () => {
+      const data = JSON.parse(req.responseText);
+      console.log(data);
+      this.setState({
+        news: data
+      });
+    };
+  }
+
+  render() {
+    return (
+      <Paper>
+        <h2>News about</h2>
+        <Grid container direction="column"></Grid>
+      </Paper>
+    );
+  }
+}
+
+export default News;
