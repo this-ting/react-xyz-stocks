2b6ed1fb7c9e46abd27817583313f853945bc0e9
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-02 18:35:40 +0800

    Set up Stock components

--- src/Markets/Stock.js
+++ src/Markets/Stock.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PrevDayPrice from './Stock-PrevDayPrice.js';
+import Company from './Stock-company.js';
+import News from './Stock-news.js';
+import Similar from './Stock-similar.js';
 
-class Stock extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Stock PAGE</h2>
-      </div>
-    );
-  }
+export default function Stock() {
+  return (
+    <div>
+      <h1>Stock PAGE</h1>
+      <h2>Chart</h2>
+      <h2>Latest Quote</h2>
+      <PrevDayPrice />
+      <Company />
+      <News />
+      <Similar />
+    </div>
+  );
 }
-
-export default Stock;
