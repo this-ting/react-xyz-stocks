2b7c117861eb1586db83c61b22d822f137212083
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-03 18:08:44 +0800

    Update header active tab

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { AppBar, Tab, Tabs } from '@material-ui/core';
 
-export default function Header() {
+function Header() {
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
   return (
-    <>
-      <AppBar position="static" className="header">
-        <Tabs variant="fullWidth" value={0}>
-          <Tab label="Home" value="0" to="/" component={NavLink} />
-          <Tab label="Markets" value="1" to="/markets/" component={NavLink} />
-          <Tab label="User" value="2" to="/user/" component={NavLink} />
-        </Tabs>
-      </AppBar>
-    </>
+    <AppBar position="static" className="header">
+      <Tabs variant="fullWidth" value={value} onChange={handleChange}>
+        <Tab label="Home" to="/" component={NavLink} />
+        <Tab label="Markets" to="/markets/" component={NavLink} />
+        <Tab label="User" to="/user/" component={NavLink} />
+      </Tabs>
+    </AppBar>
   );
 }
+
+export default Header;
