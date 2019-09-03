532e57f1f33e0ca69979e60c734877af151529ac
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-03 18:09:46 +0800

    Add tab header for stock page

--- src/Markets/Stock.js
+++ src/Markets/Stock.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import PrevDayPrice from './Stock-PrevDayPrice.js';
-import Company from './Stock-company.js';
-import News from './Stock-news.js';
-import Similar from './Stock-similar.js';
+import { Link } from 'react-router-dom';
+import { Tabs, Tab } from '@material-ui/core';
+
+// Import Components
+import Overview from './Stock-overview.js';
 
 export default function Stock() {
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
   return (
     <div>
       <h1>Stock PAGE</h1>
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="Overview" />
+        <Tab label="Financials" />
+        <Tab label="News" />
+      </Tabs>
       <h2>Chart</h2>
       <h2>Latest Quote</h2>
-      <PrevDayPrice />
-      <Company />
-      <News />
-      <Similar />
     </div>
   );
 }
