00c04547987d84e785141bd01eeee324a88a617e
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-04 19:56:24 +0800

    Set up initial Dividends component for Financials

--- src/Markets/Stock-financials.js
+++ src/Markets/Stock-financials.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // Import Components
 import Statements from './Stock-financials-statements.js';
+import Dividends from './Stock-financials-dividends.js';
 
 export default function Financials() {
   return (
     <>
       <Statements />
+      <Dividends />
     </>
   );
 }
