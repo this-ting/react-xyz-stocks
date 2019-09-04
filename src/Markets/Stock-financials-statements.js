ad3af2ce7895837b47a94cd92db529f33459a395
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-04 19:32:41 +0800

    Set up IS component for Statements component

--- src/Markets/Stock-financials-statements.js
+++ src/Markets/Stock-financials-statements.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { Paper, Grid } from '@material-ui/core';
 
 // Import Components
+import IS from './Stock-financials-statements-is.js';
 import BS from './Stock-financials-statements-bs.js';
+import CFS from './Stock-financials-statements-cfs.js';
 
 class Statements extends Component {
   state = {};
@@ -11,7 +13,9 @@ class Statements extends Component {
     return (
       <Paper>
         <h2>Quarterly Financials</h2>
+        <IS />
         <BS />
+        <CFS />
       </Paper>
     );
   }
