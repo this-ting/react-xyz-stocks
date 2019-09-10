130981731be220792bb4949783abf00e3a61adf5
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-10 08:34:59 +0800

    Change IS component to use hooks

--- src/Markets/Stock-financials-statements-is.js
+++ src/Markets/Stock-financials-statements-is.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Paper, Grid } from '@material-ui/core';
+import StockContext from './StockContext.js';
 
-class IS extends Component {
-  state = {
+const IS = () => {
+  // context
+  const input = useContext(StockContext);
+
+  const initialState = {
     is: [
       {
         rev: '',
@@ -47,44 +51,33 @@ class IS extends Component {
     ]
   };
 
-  componentDidMount() {
-    const req = new XMLHttpRequest();
-    req.open(
-      'get',
-      `https://financialmodelingprep.com/api/v3/financials/income-statement/AAPL?period=quarter`
-    );
-    req.send();
-    req.onload = () => {
-      const data = JSON.parse(req.responseText).financials;
-      const info = data.slice(0, 4);
-      const is = [];
-      for (let i = 0; i < info.length; i++) {
-        is[i] = {};
-        is[i].rev = info[i]['Revenue'];
-        is[i].costOfRev = info[i]['Cost of Revenue'];
-        is[i].gp = info[i]['Gross Profit'];
-        is[i].gm = info[i]['Gross Margin'];
-        is[i].opEx = info[i]['Operating Expenses'];
-        is[i].opIncome = info[i]['Operating Income'];
-        is[i].ni = info[i]['Net Income'];
-        is[i].npm = info[i]['Net Profit Margin'];
-      }
-      this.setState({ is });
-    };
-  }
+  const [income, setIncome] = useState(initialState);
+  useEffect(() => {
+    fetch(
+      `https://financialmodelingprep.com/api/v3/financials/income-statement/${input}?period=quarter`
+    )
+      .then(response => response.json())
+      .then(data => {
+        const info = data.financials.slice(0, 4);
+        const is = [];
+        for (let i = 0; i < info.length; i++) {
+          is[i] = {};
+          is[i].rev = info[i]['Revenue'];
+          is[i].costOfRev = info[i]['Cost of Revenue'];
+          is[i].gp = info[i]['Gross Profit'];
+          is[i].gm = info[i]['Gross Margin'];
+          is[i].opEx = info[i]['Operating Expenses'];
+          is[i].opIncome = info[i]['Operating Income'];
+          is[i].ni = info[i]['Net Income'];
+          is[i].npm = info[i]['Net Profit Margin'];
+        }
+        setIncome(is);
+      })
+      .catch(error => alert(error));
+  }, [input]);
 
-  render() {
-    const {
-      rev,
-      costOfRev,
-      gp,
-      gm,
-      opEx,
-      opIncome,
-      ni,
-      npm
-    } = this.state.is[0];
-
+  if (income[0]) {
+    const { rev, costOfRev, gp, gm, opEx, opIncome, ni, npm } = income[0];
     return (
       <Paper>
         <Grid container direction="column">
@@ -103,6 +96,11 @@ class IS extends Component {
       </Paper>
     );
   }
-}
+  return (
+    <>
+      <h1>LOADING</h1>
+    </>
+  );
+};
 
 export default IS;
