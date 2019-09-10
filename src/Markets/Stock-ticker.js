b165e9a9634a67966dc0c98671eef36bd0384c8e
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-10 18:05:48 +0800

    Add mount component checker for Ticker component

--- src/Markets/Stock-ticker.js
+++ src/Markets/Stock-ticker.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import StockContext from './StockContext.js';
 
 const Ticker = () => {
   // context
   const input = useContext(StockContext);
+
+  // check for component mount
+  const mounted = useRef(false);
 
   const initialState = {
     name: '',
@@ -14,6 +17,7 @@ const Ticker = () => {
   const [company, setCompany] = useState(initialState);
 
   useEffect(() => {
+    mounted.current = true;
     fetch(`https://financialmodelingprep.com/api/v3/company/profile/${input}`)
       .then(response => response.json())
       .then(data => {
@@ -21,21 +25,34 @@ const Ticker = () => {
         info.name = data.profile.companyName;
         info.exchange = data.profile.exchange;
         info.ticker = data.symbol;
-        setCompany(info);
+        if (mounted.current) {
+          setCompany(info);
+        }
       })
       .catch(error => alert(error));
+
+    // Cleanup
+    return () => {
+      mounted.current = false;
+    };
   }, [input]);
 
-  if (company) {
+  if (company.name) {
+    const { name, exchange, ticker } = company;
     return (
       <>
-        <h1>{company.name}</h1>
+        <h1>{name}</h1>
         <h2>
-          {company.exchange}: {company.ticker}
+          {exchange}: {ticker}
         </h2>
       </>
     );
   }
+  return (
+    <>
+      <h1>LOADING</h1>
+    </>
+  );
 };
 
 export default Ticker;
