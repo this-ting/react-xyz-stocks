580e26bc3849dd6bf78e349d6ddb0140f8749944
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-10 08:35:34 +0800

    Change GraphStats component to use hooks

--- src/Markets/Stock-overview-graph-stats.js
+++ src/Markets/Stock-overview-graph-stats.js
@@ -1,71 +1,117 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Paper, Grid } from '@material-ui/core';
 
-class GraphStats extends Component {
-  state = {
-    marketcap: '',
-    week52high: '',
-    week52low: '',
-    ttmEPS: '',
-    dividendYield: '',
-    peRatio: '',
-    beta: ''
-  };
+import StockContext from './StockContext.js';
 
-  componentDidMount() {
-    // IEX API usage is cheaper if you GET a single value at a time, so set up a for loop to get only the values needed
-    const stats = [
-      'marketcap',
-      'week52high',
-      'week52low',
-      'ttmEPS',
-      'dividendYield',
-      'peRatio',
-      'beta'
-    ];
-    for (let i = 0; i < stats.length; i++) {
-      let key = stats[i];
-      const req = new XMLHttpRequest();
-      req.open(
-        'get',
-        `https://sandbox.iexapis.com/stable/stock/aapl/stats/${key}?token=Tpk_7190efa09280470180ab8bb6635da780`
-      );
-      req.send();
-      req.onload = () => {
-        const data = JSON.parse(req.responseText);
-        const info = {};
-        info[key] = data;
-        this.setState(info);
-      };
-    }
-  }
+const GraphStats = () => {
+  // context
+  const input = useContext(StockContext);
 
-  render() {
-    const {
-      marketcap,
-      ttmEPS,
-      beta,
-      peRatio,
-      dividendYield,
-      week52high,
-      week52low
-    } = this.state;
+  // market cap
+  const [marketcap, setMarketCap] = useState('');
+  useEffect(() => {
+    fetch(
+      `https://sandbox.iexapis.com/stable/stock/${input}/stats/marketcap?token=Tpk_7190efa09280470180ab8bb6635da780`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setMarketCap(data);
+      });
+    // .catch(error => alert(error));
+  }, [input]);
 
-    return (
-      <Paper>
-        <h2>Graph Stats</h2>
-        <Grid container direction="column">
-          <Grid item>Market Cap: {marketcap}</Grid>
-          <Grid item>EPS: ${ttmEPS}</Grid>
-          <Grid item>Beta: {beta}</Grid>
-          <Grid item>P/E ratio: {peRatio}</Grid>
-          <Grid item>Div Yield: {dividendYield}</Grid>
-          <Grid item>52-week Low: {week52low}</Grid>
-          <Grid item>52-week High: {week52high}</Grid>
-        </Grid>
-      </Paper>
-    );
-  }
-}
+  // week 52 high
+  const [week52high, setWeek52High] = useState('');
+  useEffect(() => {
+    fetch(
+      `https://sandbox.iexapis.com/stable/stock/${input}/stats/week52high?token=Tpk_7190efa09280470180ab8bb6635da780`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setWeek52High(data);
+      });
+    // .catch(error => alert(error));
+  }, [input]);
+
+  // week 52 low
+  const [week52low, setWeek52Low] = useState('');
+  useEffect(() => {
+    fetch(
+      `https://sandbox.iexapis.com/stable/stock/${input}/stats/week52low?token=Tpk_7190efa09280470180ab8bb6635da780`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setWeek52Low(data);
+      });
+    // .catch(error => alert(error));
+  }, [input]);
+
+  // ttmEPS
+  const [ttmEPS, setTtmEPS] = useState('');
+  useEffect(() => {
+    fetch(
+      `https://sandbox.iexapis.com/stable/stock/${input}/stats/ttmEPS?token=Tpk_7190efa09280470180ab8bb6635da780`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setTtmEPS(data);
+      });
+    // .catch(error => alert(error));
+  }, [input]);
+
+  // dividendYield
+  const [dividendYield, setDividendYield] = useState('');
+  useEffect(() => {
+    fetch(
+      `https://sandbox.iexapis.com/stable/stock/${input}/stats/dividendYield?token=Tpk_7190efa09280470180ab8bb6635da780`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setDividendYield(data);
+      });
+    // .catch(error => alert(error));
+  }, [input]);
+
+  // PE ratio
+  const [peRatio, setPEratio] = useState('');
+  useEffect(() => {
+    fetch(
+      `https://sandbox.iexapis.com/stable/stock/${input}/stats/peRatio?token=Tpk_7190efa09280470180ab8bb6635da780`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setPEratio(data);
+      });
+    // .catch(error => alert(error));
+  }, [input]);
+
+  // beta
+  const [beta, setBeta] = useState('');
+  useEffect(() => {
+    fetch(
+      `https://sandbox.iexapis.com/stable/stock/${input}/stats/beta?token=Tpk_7190efa09280470180ab8bb6635da780`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setBeta(data);
+      });
+    // .catch(error => alert(error));
+  }, [input]);
+
+  return (
+    <Paper>
+      <h2>Graph Stats</h2>
+      <Grid container direction="column">
+        <Grid item>Market Cap: {marketcap}</Grid>
+        <Grid item>EPS: ${ttmEPS}</Grid>
+        <Grid item>Beta: {beta}</Grid>
+        <Grid item>P/E ratio: {peRatio}</Grid>
+        <Grid item>Div Yield: {dividendYield}</Grid>
+        <Grid item>52-week Low: {week52low}</Grid>
+        <Grid item>52-week High: {week52high}</Grid>
+      </Grid>
+    </Paper>
+  );
+};
 
 export default GraphStats;
