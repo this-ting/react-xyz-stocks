3066559720c8277cbaa0c266fe29b677ac10b8f5
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-11 11:09:58 +0800

    Move around News component

--- src/Markets/Stock-overview.js
+++ src/Markets/Stock-overview.js
@@ -4,17 +4,14 @@ import React from 'react';
 import GraphStats from './Stock-overview-graph-stats.js';
 import PrevDayPrice from './Stock-overview-PrevDayPrice.js';
 import Company from './Stock-overview-company.js';
-import News from './Stock-overview-news.js';
 import KeyFin from './Stock-overview-keyfin.js';
 
 const Overview = () => {
   return (
     <div>
-      <h2>Overview</h2>
       <GraphStats />
       <PrevDayPrice />
       <Company />
-      <News />
       <KeyFin />
     </div>
   );
