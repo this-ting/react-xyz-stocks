a757921658ab362ca255c696a0f11bc6f4dc438d
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-16 20:46:43 +0800

    Set up inital list for explore

--- src/Markets/Explore-explorelist.js
+++ src/Markets/Explore-explorelist.js
@@ -1,4 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from '@material-ui/core';
 // import * as firebase from 'firebase/app';
 import { db } from '../Firebase.js';
 
@@ -13,8 +22,20 @@ import { db } from '../Firebase.js';
 //   });
 // };
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto'
+  },
+  table: {
+    minWidth: 650
+  }
+}));
+
 const ExploreList = props => {
   const { sector } = props;
+  const classes = useStyles();
 
   // state for previous price
   const [price, setPrice] = useState('');
@@ -67,7 +88,7 @@ const ExploreList = props => {
           for (let i = 0; i < info.length; i++) {
             const ticker = Object.keys(data)[i];
             const percent = data[ticker].chart[4].changePercent;
-            info[i].changePercent = percent;
+            info[i].percent = percent;
           }
           setPrevious(info);
         })
@@ -75,9 +96,54 @@ const ExploreList = props => {
     }
   }, [price]);
 
+  const companyList = () => {
+    if (previous) {
+      previous.map(prev => (
+        <TableRow key={prev.name}>
+          <TableCell component="th" scope="row">
+            {prev.name}
+          </TableCell>
+          <TableCell align="right">{prev.close}</TableCell>
+          <TableCell align="right">{prev.percent}</TableCell>
+        </TableRow>
+      ));
+    }
+  };
+
+  const companyChart = () => {
+    if (previous) {
+      return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Company</TableCell>
+                <TableCell align="right">Price</TableCell>
+                <TableCell align="right">% Change</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{companyList}</TableBody>
+          </Table>
+        </Paper>
+      );
+    }
+  };
+
   return (
     <>
       <h3>LIST OF COMPANIES IN {sector.toUpperCase()}</h3>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Company</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">% Change</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{companyList}</TableBody>
+        </Table>
+      </Paper>
     </>
   );
 };
