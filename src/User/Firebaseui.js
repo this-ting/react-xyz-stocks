e0a7832e919106018907c04e2b9228aea75761cd
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-16 10:48:43 +0800

    Add Firebase component

--- src/User/Firebaseui.js
+++ src/User/Firebaseui.js
@@ -2,19 +2,7 @@ import React, { Component } from 'react';
 import { Typography } from '@material-ui/core';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import * as firebase from 'firebase/app';
-import 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDdbfaxWHfyLR22bK6_sJPrH0-rHNAyNWU',
-  authDomain: 'react-xyz-stocks.firebaseapp.com',
-  databaseURL: 'https://react-xyz-stocks.firebaseio.com',
-  projectId: 'react-xyz-stocks',
-  storageBucket: 'react-xyz-stocks.appspot.com',
-  messagingSenderId: '309335633325',
-  appId: '1:309335633325:web:a8321e55ca9ef500'
-};
-
-firebase.initializeApp(firebaseConfig);
+import { auth } from '../Firebase.js';
 
 class SignInScreen extends Component {
   // The component's Local state.
@@ -40,9 +28,9 @@ class SignInScreen extends Component {
 
   // Listen to the Firebase Auth state and set the local state.
   componentDidMount() {
-    this.unregisterAuthObserver = firebase
-      .auth()
-      .onAuthStateChanged(user => this.setState({ isSignedIn: !!user }));
+    this.unregisterAuthObserver = auth.onAuthStateChanged(user =>
+      this.setState({ isSignedIn: !!user })
+    );
   }
 
   // Make sure we un-register Firebase observers when the component unmounts.
@@ -57,10 +45,7 @@ class SignInScreen extends Component {
           <Typography variant="h5" color="textPrimary">
             Sign in to XYZ Stocks
           </Typography>
-          <StyledFirebaseAuth
-            uiConfig={this.uiConfig}
-            firebaseAuth={firebase.auth()}
-          />
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={auth} />
         </div>
       );
     }
@@ -69,11 +54,8 @@ class SignInScreen extends Component {
         <Typography variant="h5" color="textPrimary">
           XYZ Stocks
         </Typography>
-        <p>
-          Welcome {firebase.auth().currentUser.displayName}! You are now
-          signed-in!
-        </p>
-        <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+        <p>Welcome {auth.currentUser.displayName}! You are now signed-in!</p>
+        <a onClick={() => auth.signOut()}>Sign-out</a>
       </div>
     );
   }
