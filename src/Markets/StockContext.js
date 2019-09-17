d9a00f2d95c330e7f50f0bbad5ecb6497711d62a
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-17 18:53:39 +0800

    Update context comments

--- src/Markets/StockContext.js
+++ src/Markets/StockContext.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
+// create new context
 const StockContext = React.createContext({});
 
+// wrapper where you want access to the context
 export const StockProvider = StockContext.Provider;
-export const StockConsumer = StockContext.Consumer;
 
 export default StockContext;
