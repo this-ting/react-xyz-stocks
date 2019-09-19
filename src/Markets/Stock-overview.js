5b44925806835b4aef137ffd8af70a7ffd34fa90
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-19 21:07:59 +0800

    Move GraphStats from Overview to GraphContainer

--- src/Markets/Stock-overview.js
+++ src/Markets/Stock-overview.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 // Import Components
-import GraphStats from './Stock-overview-graph-stats.js';
 import PrevDayPrice from './Stock-overview-PrevDayPrice.js';
 import Company from './Stock-overview-company.js';
 import KeyFin from './Stock-overview-keyfin.js';
@@ -9,7 +8,6 @@ import KeyFin from './Stock-overview-keyfin.js';
 const Overview = () => {
   return (
     <div>
-      <GraphStats />
       <PrevDayPrice />
       <Company />
       <KeyFin />
