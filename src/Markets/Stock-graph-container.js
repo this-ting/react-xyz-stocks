6859f0afec38e1051489ea5502ab968e93664ab6
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-19 21:08:32 +0800

    Set up time period tabs for graph

---
+++ src/Markets/Stock-graph-container.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import { Tabs, Tab, Typography, Box } from '@material-ui/core';
+
+// import components
+import Graph from './Stock-graph.js';
+import GraphStats from './Stock-graph-graph-stats.js';
+
+const TabPanel = props => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`graph-tabpanel-${index}`}
+      aria-labelledby={`graph-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+};
+
+function a11yProps(index) {
+  return {
+    id: `graph-tab-${index}`,
+    'aria-controls': `graph-tabpanel-${index}`
+  };
+}
+
+const GraphContainer = () => {
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  if (value === 0) {
+    return (
+      <>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="graph length tabs example"
+        >
+          <Tab label="5d" {...a11yProps(0)} />
+          <Tab label="1m" {...a11yProps(1)} />
+          <Tab label="3m" {...a11yProps(2)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          <Graph time="5d" />
+        </TabPanel>
+        <GraphStats />
+      </>
+    );
+  }
+  if (value === 1) {
+    return (
+      <>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="graph length tabs example"
+        >
+          <Tab label="5d" {...a11yProps(0)} />
+          <Tab label="1m" {...a11yProps(1)} />
+          <Tab label="3m" {...a11yProps(2)} />
+        </Tabs>
+        <TabPanel value={value} index={1}>
+          <Graph time="1m" />
+        </TabPanel>
+        <GraphStats />
+      </>
+    );
+  }
+  if (value === 2) {
+    return (
+      <>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="graph length tabs example"
+        >
+          <Tab label="5d" {...a11yProps(0)} />
+          <Tab label="1m" {...a11yProps(1)} />
+          <Tab label="3m" {...a11yProps(2)} />
+        </Tabs>
+        <TabPanel value={value} index={2}>
+          <Graph time="3m" />
+        </TabPanel>
+        <GraphStats />
+      </>
+    );
+  }
+
+  // return (
+  //   <>
+  //     <Tabs
+  //       value={value}
+  //       onChange={handleChange}
+  //       aria-label="graph length tabs example"
+  //     >
+  //       <Tab label="5d" {...a11yProps(0)} />
+  //       <Tab label="1m" {...a11yProps(1)} />
+  //       <Tab label="3m" {...a11yProps(2)} />
+  //     </Tabs>
+  //     <GraphStats />
+  //   </>
+  // );
+};
+
+export default GraphContainer;
