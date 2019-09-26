54de2ee2f4d794678f4580c290b848b8d85f4398
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-26 16:42:35 +0800

    Move state up to Portfolio

--- src/User/portfolio-watchlist.js
+++ src/User/portfolio-watchlist.js
@@ -12,53 +12,7 @@ import {
 import { makeStyles } from '@material-ui/styles';
 import { db } from '../Firebase.js';
 
-const Watchlist = () => {
-  // check for mounted component
-  const mounted = useRef(false);
-  const data = [];
-  const user = 'PuqnfR7XgVU6N4EknUxZ';
-
-  const [watchItems, setWatchItems] = useState('');
-  useEffect(() => {
-    mounted.current = true;
-
-    // get user's following stocks from FireStore, then fetch API
-    db.collection('users')
-      .doc(user)
-      .collection('stocks')
-      .get()
-      .then(query => {
-        query.forEach(doc => {
-          data.push(doc.data());
-        });
-        return data;
-      })
-      .then(data => {
-        let loaded = 0;
-        for (let i = 0; i < data.length; i++) {
-          fetch(
-            `https://sandbox.iexapis.com/stable/stock/${data[i].ticker}/previous?filter=date,close,changePercent,change&token=Tpk_7190efa09280470180ab8bb6635da780`
-          )
-            .then(response => response.json())
-            .then(apiData => {
-              data[i].date = apiData.date;
-              data[i].close = apiData.close;
-              data[i].changePercent = apiData.changePercent.toFixed(2);
-              data[i].change = apiData.change.toFixed(2);
-              loaded++;
-              if (loaded >= data.length) {
-                setWatchItems(data);
-              }
-            });
-        }
-      })
-      .catch(error => alert(error));
-
-    return () => {
-      mounted.current = false;
-    };
-  }, [user]);
-
+const Watchlist = ({ following }) => {
   return (
     <Container>
       <Typography variant="h6" gutterBottom="true">
@@ -77,18 +31,18 @@ const Watchlist = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {watchItems
-            ? watchItems.map(items => {
+          {following
+            ? following.map(follow => {
                 return (
                   <TableRow>
-                    <TableCell>{items.ticker}</TableCell>
-                    <TableCell>{items.company}</TableCell>
-                    <TableCell>{items.close}</TableCell>
-                    <TableCell>{items.changePercent}</TableCell>
-                    <TableCell>{items.change}</TableCell>
+                    <TableCell>{follow.ticker}</TableCell>
+                    <TableCell>{follow.company}</TableCell>
+                    <TableCell>{follow.close}</TableCell>
+                    <TableCell>{follow.changePercent}</TableCell>
+                    <TableCell>{follow.change}</TableCell>
                     <TableCell>
                       {(
-                        ((items.entryPrice - items.close) / items.close) *
+                        ((follow.entryPrice - follow.close) / follow.close) *
                         100
                       ).toFixed(2)}
                     </TableCell>
--- src/User/portfolio.js
+++ src/User/portfolio.js
@@ -9,9 +9,11 @@ import {
   TableCell
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import { db } from '../Firebase.js';
 
 // import components
 import Watchlist from './portfolio-watchlist.js';
+import PortfolioNews from './portfolio-news.js';
 
 const useStyles = makeStyles({
   root: {
@@ -22,12 +24,77 @@ const useStyles = makeStyles({
 
 const Portfolio = () => {
   const classes = useStyles();
+
+  // check for mounted component
+  const mounted = useRef(false);
+
+  const data = [];
+  const user = 'PuqnfR7XgVU6N4EknUxZ';
+
+  const [following, setFollowing] = useState('');
+  useEffect(() => {
+    mounted.current = true;
+    // get user's following stocks from FireStore, then fetch API
+    db.collection('users')
+      .doc(user)
+      .collection('stocks')
+      .get()
+      .then(query => {
+        query.forEach(doc => {
+          data.push(doc.data());
+        });
+        return data;
+      })
+      .then(data => {
+        let loaded = 0;
+        for (let i = 0; i < data.length; i++) {
+          fetch(
+            `https://sandbox.iexapis.com/stable/stock/${data[i].ticker}/previous?filter=date,close,changePercent,change&token=Tpk_7190efa09280470180ab8bb6635da780`
+          )
+            .then(response => response.json())
+            .then(apiData => {
+              data[i].date = apiData.date;
+              data[i].close = apiData.close;
+              data[i].changePercent = apiData.changePercent.toFixed(2);
+              data[i].change = apiData.change.toFixed(2);
+              loaded++;
+              if (loaded >= data.length) {
+                setFollowing(data);
+              }
+            });
+        }
+      })
+      .catch(error => console.error(error));
+
+    return () => {
+      mounted.current = false;
+    };
+  }, [user]);
+
+  const [companies, setCompanies] = useState('');
+  useEffect(() => {
+    mounted.current = true;
+
+    db.collection('users')
+      .doc(user)
+      .get()
+      .then(doc => {
+        setCompanies(doc.data().watchlist);
+      })
+      .catch(error => console.error(error));
+
+    return () => {
+      mounted.current = false;
+    };
+  }, [user]);
+
   return (
     <Container className={classes.root}>
       <Typography variant="h6" gutterBottom="true">
         Portfolio
       </Typography>
-      <Watchlist />
+      <Watchlist following={following} />
+      <PortfolioNews companies={companies} />
     </Container>
   );
 };
