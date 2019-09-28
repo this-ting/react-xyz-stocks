a3d64005567a832b83a5f9cad98da9c994cba2ef
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-28 13:54:07 +0800

    Update UI of News component with RWD

--- src/Markets/Stock-news.js
+++ src/Markets/Stock-news.js
@@ -1,24 +1,58 @@
 import React, { useState, useEffect, useContext, useRef } from 'react';
-import { Paper, Grid, Typography } from '@material-ui/core';
+import {
+  Paper,
+  Grid,
+  Typography,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Hidden
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import StockContext from './StockContext.js';
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 600,
+    margin: '1rem 0'
+  },
+  root2: {
+    maxWidth: 980,
+    margin: '0.5rem 0'
+  },
+  media: {
+    maxWidth: 580,
+    height: 140
+  },
+  media2: {
+    maxWidth: 345,
+    height: 170
+  },
+  header: {
+    maxWidth: 345,
+    height: 140
+  },
+  link: {
+    textDecoration: 'none'
+  }
+});
+
 const News = () => {
+  const classes = useStyles();
+
   // context
   const input = useContext(StockContext);
 
   // check for component mount
   const mounted = useRef(false);
 
-  const initialState = {
-    news: []
-  };
-
-  const [news, setNews] = useState(initialState);
+  const [news, setNews] = useState('');
 
   useEffect(() => {
     mounted.current = true;
     fetch(
-      `https://sandbox.iexapis.com/stable/stock/${input}/news/last/3?filter=datetime,headline,source,url,image,related&token=Tpk_7190efa09280470180ab8bb6635da780`
+      `https://cloud.iexapis.com/stable/stock/${input}/news/last/5?filter=datetime,headline,source,url,image,related,summary&token=pk_0c6bc8f3cc794020a71b34f4fda09669`
     )
       .then(response => response.json())
       .then(data => {
@@ -26,7 +60,7 @@ const News = () => {
           setNews(data);
         }
       })
-      .catch(error => alert(error));
+      .catch(error => console.error(error));
 
     // Cleanup
     return () => {
@@ -34,70 +68,106 @@ const News = () => {
     };
   }, [input]);
 
-  if (news[0]) {
+  if (news) {
     const articles = news.map(n => (
-      <Paper>
-        <Grid container direction="row">
-          <Grid container direction="column">
-            <Grid item xs={6}>
-              <Typography varitant="h6" gutterBottom="true">
-                <a href={n.url}>{n.headline}</a>
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography varitant="overline" color="textSecondary">
-                {n.source} - {n.datetime}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container align="right">
-            <img
-              src={n.image}
-              style={({ height: '150px' }, { width: '150px' })}
-              alt="news article"
-            />
-          </Grid>
-        </Grid>
-      </Paper>
+      <>
+        <Hidden smUp>
+          <Card className={classes.root}>
+            <a href={n.url} className={classes.link} target="_blank">
+              <CardActionArea>
+                <CardMedia image={n.image} className={classes.media} />
+                <CardContent>
+                  <Typography variant="h6" color="textSecondary" component="h6">
+                    {n.headline}
+                  </Typography>
+                  {/* <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {news.summary}
+                  </Typography> */}
+                  <Grid container spacing={2}>
+                    <Grid item>
+                      <Typography
+                        variant="overline"
+                        color="textSecondary"
+                        component="span"
+                      >
+                        {n.source}
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography
+                        variant="overline"
+                        color="textSecondary"
+                        component="span"
+                      >
+                        {new Date(n.datetime).toGMTString()}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </CardActionArea>
+            </a>
+          </Card>
+        </Hidden>
+
+        <Hidden xsDown>
+          <Card className={classes.root2}>
+            <a href={n.url} className={classes.link} target="_blank">
+              <CardActionArea>
+                <Grid container wrap="wrap">
+                  <Grid item sm={3}>
+                    <CardMedia image={n.image} className={classes.media2} />
+                  </Grid>
+                  <Grid item sm={9}>
+                    <CardContent>
+                      <Typography
+                        variant="h6"
+                        color="textSecondary"
+                        component="h6"
+                      >
+                        {n.headline}
+                      </Typography>
+                      <Grid container spacing={2}>
+                        <Grid item>
+                          <Typography
+                            variant="overline"
+                            color="textSecondary"
+                            component="span"
+                          >
+                            {n.source}
+                          </Typography>
+                        </Grid>
+                        <Grid item>
+                          <Typography
+                            variant="overline"
+                            color="textSecondary"
+                            component="span"
+                          >
+                            {new Date(n.datetime).toGMTString()}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        {n.summary}
+                      </Typography>
+                    </CardContent>
+                  </Grid>
+                </Grid>
+              </CardActionArea>
+            </a>
+          </Card>
+        </Hidden>
+      </>
     ));
 
-    const testArticle = (
-      <Paper>
-        <Grid container direction="row">
-          <Grid container direction="column">
-            <Grid item xs={9}>
-              <Typography varitant="h6" gutterBottom="true">
-                <a href="https://cloud.iexapis.com/v1/news/article/8aceb77d-cde7-4ffa-9e66-7253cc6b8789">
-                  The latest Apple AirPods are down to their lowest price ever
-                </a>
-              </Typography>
-            </Grid>
-            <Grid item xs={9}>
-              <Typography varitant="overline" color="textSecondary">
-                USA Today Tue, 03 Sep 2019 21:08:32 GMT
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container align="right">
-            <img
-              src="https://cloud.iexapis.com/v1/news/image/8aceb77d-cde7-4ffa-9e66-7253cc6b8789"
-              style={({ height: '150px' }, { width: '150px' })}
-              alt="news article"
-            />
-          </Grid>
-        </Grid>
-      </Paper>
-    );
-
-    return (
-      <Paper>
-        <h2>News about</h2>
-        <Grid container direction="column">
-          {testArticle}
-          {articles}
-        </Grid>
-      </Paper>
-    );
+    return <>{articles}</>;
   }
 
   return (
