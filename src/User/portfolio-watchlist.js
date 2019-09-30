1f03d4d100656a7e694f871eb2c7dfb51971cc5d
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-09-30 16:38:24 +0800

    Change conditional rendering method

--- src/User/portfolio-watchlist.js
+++ src/User/portfolio-watchlist.js
@@ -13,6 +13,9 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import CloseIcon from '@material-ui/icons/Close';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+
 import { db } from '../Firebase.js';
 
 const Watchlist = ({
@@ -27,6 +30,59 @@ const Watchlist = ({
     getCompany(input);
   };
 
+  const renderRows = following
+    ? following.map(follow => {
+        return (
+          <TableRow hover key={follow.ticker} onClick={handleCompanyClick}>
+            <TableCell>{follow.ticker}</TableCell>
+            <TableCell>{follow.company}</TableCell>
+            <TableCell>{follow.close}</TableCell>
+            <TableCell>{follow.changePercent}</TableCell>
+            <TableCell>{follow.change}</TableCell>
+            {/* <TableCell>
+              {(
+                ((follow.entryPrice - follow.close) / follow.close) *
+                100
+              ).toFixed(2)}
+            </TableCell> */}
+            <TableCell>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleDelete}
+                {...{ ticker: follow.ticker }}
+              >
+                Remove
+              </Button>
+              <Snackbar
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                open={open}
+                autoHideDuration={3000}
+                onClose={handleClose}
+                ContentProps={{
+                  'aria-describedby': 'message-id'
+                }}
+                message={<span id="message-id">Removed from Watchlist!</span>}
+                action={[
+                  <IconButton
+                    key="close"
+                    aria-label="close"
+                    color="inherit"
+                    onClick={handleClose}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                ]}
+              />
+            </TableCell>
+          </TableRow>
+        );
+      })
+    : null;
+
   return (
     <>
       <Typography variant="h6" gutterBottom>
@@ -38,72 +94,13 @@ const Watchlist = ({
             <TableCell>Symbol</TableCell>
             <TableCell>Company</TableCell>
             <TableCell>Price</TableCell>
-            <TableCell>% change</TableCell>
-            <TableCell>Change</TableCell>
+            <TableCell>% Change</TableCell>
+            <TableCell>$ Change</TableCell>
             {/* <TableCell>Return</TableCell> */}
             <TableCell>Remove</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {following
-            ? following.map(follow => {
-                return (
-                  <TableRow
-                    hover
-                    key={follow.ticker}
-                    onClick={handleCompanyClick}
-                  >
-                    <TableCell>{follow.ticker}</TableCell>
-                    <TableCell>{follow.company}</TableCell>
-                    <TableCell>{follow.close}</TableCell>
-                    <TableCell>{follow.changePercent}</TableCell>
-                    <TableCell>{follow.change}</TableCell>
-                    {/* <TableCell>
-                      {(
-                        ((follow.entryPrice - follow.close) / follow.close) *
-                        100
-                      ).toFixed(2)}
-                    </TableCell> */}
-                    <TableCell>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={handleDelete}
-                        {...{ ticker: follow.ticker }}
-                      >
-                        Remove
-                      </Button>
-                      <Snackbar
-                        anchorOrigin={{
-                          vertical: 'top',
-                          horizontal: 'right'
-                        }}
-                        open={open}
-                        autoHideDuration={3000}
-                        onClose={handleClose}
-                        ContentProps={{
-                          'aria-describedby': 'message-id'
-                        }}
-                        message={
-                          <span id="message-id">Removed from Watchlist!</span>
-                        }
-                        action={[
-                          <IconButton
-                            key="close"
-                            aria-label="close"
-                            color="inherit"
-                            onClick={handleClose}
-                          >
-                            <CloseIcon />
-                          </IconButton>
-                        ]}
-                      />
-                    </TableCell>
-                  </TableRow>
-                );
-              })
-            : null}
-        </TableBody>
+        <TableBody>{renderRows}</TableBody>
       </Table>
     </>
   );
