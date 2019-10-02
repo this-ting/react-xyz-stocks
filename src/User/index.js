1895d350ada7cfc675ce2bab91dc88ae019e73de
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-10-02 18:38:42 +0800

    Set up initial Settings component

--- src/User/index.js
+++ src/User/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box } from '@material-ui/core';
 import Login from './User-login.js';
+import Settings from './User-settings.js';
+import LoginContext from '../LoginContext.js';
 
 const User = () => {
+  const uid = useContext(LoginContext);
+
+  if (uid) {
+    return <Settings />;
+  }
   return (
     <Box>
       <Login />
