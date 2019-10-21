5c106e4eee0d4bfdb5b69bd5c25251f9490e873e
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-10-21 11:18:08 +0800

    Modify path for test component ExploreSectors

--- test/Explore-exploreSectors.test.js
+++ test/Explore-exploreSectors.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import ExploreSectors from '../src/Explore/Explore-exploreSectors';
+import ExploreSectors from '../src/Explore/ExploreSectors';
 
 test('render correctly', () => {
   const tree = renderer.create(<ExploreSectors />).toJSON();
